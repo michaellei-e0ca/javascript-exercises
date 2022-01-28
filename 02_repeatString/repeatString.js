6aa476d1afdb5924933246844d223ec2df3eeb7d
Michael Lei <michaelleie0ca@gmail.com>
2022-01-27 16:03:33 -0800

    Finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num == 0) {
 
+        return "";
+
+    } else if (num < 0) {
+
+        return "ERROR";
+    }
+    let newstr = "";
+    
+    for (let index = 0; index < num; index++) {
+        
+        newstr = newstr + str;
+        
+    }
+    return newstr;
 };
 
 // Do not edit below this line
