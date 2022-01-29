c047c1cafdcded947cc172c618f7f0453dfdb9ef
Michael Lei <michaelleie0ca@gmail.com>
2022-01-28 18:18:16 -0800

    Working on 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            arr.splice(i)
+        }     
+    }
+    return arr;
 };
 
 // Do not edit below this line
