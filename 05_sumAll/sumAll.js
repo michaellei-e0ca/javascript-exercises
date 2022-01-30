7329cf27f4bf667173f72f56606c03de42c87844
Michael Lei <michaelleie0ca@gmail.com>
2022-01-29 19:55:33 -0800

    Finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let numbers = [];
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    } else if (typeof(start) != "number" || typeof(end) != "number") {
+        return "ERROR";
+    }
+    if (start > end) {
+        swap = start;
+        start = end;
+        end = swap;
+    }
+    for (let i = start; i <= end; i++) {
+        numbers.push(i);
+    }
+    const reducer = (previousValue, currentValue) => previousValue + currentValue;
+    return numbers.reduce(reducer);
 };
 
 // Do not edit below this line
