1835a314be2c2d124ee535a3bc058f72128f76fb
Michael Lei <michaelleie0ca@gmail.com>
2022-02-05 15:00:11 -0800

    Finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) { 
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+  return arr.reduce(reducer, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
 
+	const reducer = (previousValue, currentValue) => previousValue * currentValue;
+
+  return arr.reduce(reducer);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(arrLength) {
+  let arr = [1];
+
+	for (let i = 0; i < arrLength; i++) {
+    arr[i] = i + 1;
+  }
+
+  const reducer = (previousValue, currentValue) => previousValue * currentValue;
+
+  return arr.reduce(reducer);
 };
 
 // Do not edit below this line
