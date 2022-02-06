5666f2be8e0498d8570ced305721097dfe19ca61
Michael Lei <michaelleie0ca@gmail.com>
2022-02-06 15:07:34 -0800

    Finished 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    bookTitles = [];
 
+    for (let i = 0; i < books.length; i++) {
+        
+        bookTitles.push(books[i].title);
+        
+    }
+    return bookTitles;
 };
 
 // Do not edit below this line
