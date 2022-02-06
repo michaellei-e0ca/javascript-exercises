1ee3f550e8ed52335724c83eee9a9e566d7b257f
Michael Lei <michaelleie0ca@gmail.com>
2022-02-06 14:48:05 -0800

    Finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) {
+        return 'OOPS';
+    }
+    if (num == 1) {
+        return 1;
+    }
+    let a = 1, b = 0, temp;
+    while (num > 1) {
+        temp = a + b; 
+        b = a; 
+        a = temp 
+        num--;
+    }
+    return temp;
 };
 
 // Do not edit below this line
