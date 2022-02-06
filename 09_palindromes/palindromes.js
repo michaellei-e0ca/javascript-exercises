e60eff7e8ecd066cbcd6fe0e04d246a2c78f8240
Michael Lei <michaelleie0ca@gmail.com>
2022-02-06 12:38:18 -0800

    Finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
 
-};
+    str = str.split(' ').join('');
+
+    let reverseStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverseStr = reverseStr + str[i];
+    }
+    
+    if (str == reverseStr) {
+        return true;
+    }
+    
+    return false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
