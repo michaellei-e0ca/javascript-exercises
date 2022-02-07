dc2238cb2445f2b0e433450e74668906abe03ff3
Michael Lei <michaelleie0ca@gmail.com>
2022-02-06 23:12:53 -0800

    Finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let yearsOld = 0, oldest = 0, oldestPerson;
+    let currentDate =  new Date().getFullYear();
+
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath !== undefined) {
+            yearsOld = people[i].yearOfDeath - people[i].yearOfBirth;
+        } else {
+             yearsOld = currentDate - people[i].yearOfBirth;
+        }
+        if (yearsOld > oldest) {
+
+            oldest = yearsOld;
+            oldestPerson = people[i];
+
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
